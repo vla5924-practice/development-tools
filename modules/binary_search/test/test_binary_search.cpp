832a92fc1933453945dbd3bd2ca32e8fee3182df
Mikhail Zoreev <43145054+BFDestroyeer@users.noreply.github.com>
2021-05-20 12:39:06 +0300

    Зореев М.В. 381806-1. ЛР3. (#309)

--- modules/binary_search/test/test_binary_search.cpp
+++ modules/binary_search/test/test_binary_search.cpp
@@ -11,7 +11,7 @@ TEST(Binary_Search_Test, IntSearch) {
     std::vector<int> array = {
         -3, -2, -1, 0, 1, 2, 3
     };
-    int result = binary_search::BinSearch<int>(0, array, 0, array.size() - 1);
+    int result = BinarySearch::Search<int>(0, array, 0, array.size() - 1);
     ASSERT_EQ(3, result);
 }
 
@@ -19,8 +19,8 @@ TEST(Binary_Search_Test, DoubleSearch) {
     std::vector<double> array = {
         -3.7, -2.8, -1.9, 0.35, 1.5, 2.89, 3.14
     };
-    int result = binary_search::BinSearch<double>(3.14, array, 0,
-                                                  array.size() - 1);
+    int result = BinarySearch::Search<double>(3.14, array, 0,
+                                              array.size() - 1);
     ASSERT_EQ(6, result);
 }
 
@@ -28,8 +28,8 @@ TEST(Binary_Search_Test, StringSearch) {
     std::vector<std::string> array = {
         "a", "b", "c", "d", "e", "f", "g"
     };
-    int result = binary_search::BinSearch<std::string>("a", array, 0,
-                                                    array.size() - 1);
+    int result = BinarySearch::Search<std::string>("a", array, 0,
+                                                   array.size() - 1);
     ASSERT_EQ(0, result);
 }
 
@@ -37,15 +37,15 @@ TEST(Binary_Search_Test, MissingValueSearch) {
     std::vector<int> array = {
         -3, -2, -1, 0, 1, 2, 3
     };
-    int result = binary_search::BinSearch<int>(10, array, 0, array.size() - 1);
+    int result = BinarySearch::Search<int>(10, array, 0, array.size() - 1);
     ASSERT_EQ(-1, result);
 }
 
-TEST(Binary_Search_Test, UnSortedSearch) {
+TEST(Binary_Search_Test, UnsortedSearch) {
     std::vector<int> array = {
         8, 2, 3, 17, -1, 27, -31
     };
-    int result = binary_search::BinSearch<int>(-1, array, 0, array.size() - 1);
+    int result = BinarySearch::Search<int>(-1, array, 0, array.size() - 1);
     ASSERT_EQ(-1, result);
 }
 
@@ -53,44 +53,46 @@ TEST(Binary_Search_Test, SearchInPartOfArray) {
     std::vector<int> array = {
         -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
     };
-    int result = binary_search::BinSearch<int>(3, array, 2, 8);
+    int result = BinarySearch::Search<int>(3, array, 2, 8);
     ASSERT_EQ(8, result);
 }
 
-TEST(Binary_Search_Test, IncorrectRight) {
+TEST(Binary_Search_Test, IncorrectRightNegative) {
     std::vector<int> array = {
         -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
     };
-    try {
-        binary_search::BinSearch<int>(3, array, 2, 27);
-        FAIL();
-    }
-    catch (const std::string msg) {
-        ASSERT_EQ("Right border out of range", msg);
-    }
+    ASSERT_THROW(BinarySearch::Search<int>(3, array, 2, -3),
+                 std::runtime_error);
 }
 
-TEST(Binary_Search_Test, IncorrectLeft) {
+TEST(Binary_Search_Test, IncorrectRightOutOfRange) {
+    std::vector<int> array = {
+            -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+    };
+    ASSERT_THROW(BinarySearch::Search<int>(3, array, 2, 27),
+                 std::runtime_error);
+}
+
+TEST(Binary_Search_Test, IncorrectLeftNegative) {
     std::vector<int> array = {
         -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
     };
-    try {
-        binary_search::BinSearch<int>(3, array, -7, 8);
-        FAIL();
-    } catch (const std::string msg) {
-        ASSERT_EQ("Left border out of range", msg);
-    }
+    ASSERT_THROW(BinarySearch::Search<int>(3, array, -7, 8),
+                 std::runtime_error);
+}
+
+TEST(Binary_Search_Test, IncorrectLeftOutOfRange) {
+    std::vector<int> array = {
+            -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
+    };
+    ASSERT_THROW(BinarySearch::Search<int>(3, array, 27, 8),
+                 std::runtime_error);
 }
 
 TEST(Binary_Search_Test, LeftGreaterThanRight) {
     std::vector<int> array = {
         -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5
     };
-
-    try {
-        binary_search::BinSearch<int>(3, array, 8, 3);
-        FAIL();
-    } catch (const std::string msg) {
-        ASSERT_EQ("Right border must be greater than left border", msg);
-    }
+    ASSERT_THROW(BinarySearch::Search<int>(3, array, 8, 3),
+                 std::runtime_error);
 }
